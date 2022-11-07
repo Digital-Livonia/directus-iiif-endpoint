93c2313658f625b4a59bc3c363532c461778a383
Hembo Pagi <95413906+HemboPagi@users.noreply.github.com>
2022-11-07 12:05:22 +0200

    manifest change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// sisse tulev väärtus on faili ID
+// selle järgi pärida faili mõõtmed directus_files tabelist ning asendada height ja width väärtused
+// kas api väljund cachetakse kuidagi? tegelit poleks vaja ju uusi päringuid teha alati ...
+
 export default {
   id: "iiif",
   handler: (router) => {
     let jsonObject = {
       "@context": "http://iiif.io/api/presentation/3/context.json",
-      id: "https://db.dl.tlu.ee/iiif/manifest/file/file_id",
+      id: "https://db.dl.tlu.ee/iiif/manifest/file/file_id", //replace <file_id> with the real value 
       type: "Manifest",
       label: {
         en: ["Image"],
@@ -13,8 +17,8 @@ export default {
         {
           id: "https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/canvas/p1",
           type: "Canvas",
-          height: 1800,
-          width: 1200,
+          height: 1800,//replace with real values
+          width: 1200,//replace with real values
           items: [
             {
               id: "https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/page/p1/1",
@@ -25,11 +29,11 @@ export default {
                   type: "Annotation",
                   motivation: "painting",
                   body: {
-                    id: "https://db.dl.tlu.ee/assets/f1667609-2d01-4c5d-98ef-7e4f3127664d",
+                    id: "https://db.dl.tlu.ee/assets/f1667609-2d01-4c5d-98ef-7e4f3127664d?format=jpg", //replace file ID with the real value and lets make sure it is JPG by using format=jpg
                     type: "Image",
                     format: "image/jpeg",
-                    height: 1800,
-                    width: 1200,
+                    height: 1800, //replace with real values
+                    width: 1200,//replace with real values
                   },
                   target:
                     "https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/canvas/p1",
