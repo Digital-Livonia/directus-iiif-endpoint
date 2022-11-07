0160156761f172d26948af0cbbe81a95474bf16b
Hembo Pagi <95413906+HemboPagi@users.noreply.github.com>
2022-11-07 11:57:51 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
-export default (router, { services, exceptions }) => {
-  const { ItemsService } = services;
-  const { ServiceUnavailableException } = exceptions;
-
-  router.get("/iiif", (req, res, next) => {
-    const recipeService = new ItemsService("directus_files", {
-      schema: req.schema,
-      accountability: req.accountability,
-    });
-
-    recipeService
-      .readByQuery({ sort: ["title"], fields: ["*"] })
-      .then((results) => res.json(results))
-      .catch((error) => {
-        return next(new ServiceUnavailableException(error.message));
-      });
-  });
-
+export default {
+  id: "iiif",
+  handler: (router) => {
+    let jsonObject = {
+      "@context": "http://iiif.io/api/presentation/3/context.json",
+      id: "https://db.dl.tlu.ee/iiif/manifest/file/file_id",
+      type: "Manifest",
+      label: {
+        en: ["Image"],
+      },
+      rights: "http://creativecommons.org/licenses/by/4.0/",
+      items: [
+        {
+          id: "https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/canvas/p1",
+          type: "Canvas",
+          height: 1800,
+          width: 1200,
+          items: [
+            {
+              id: "https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/page/p1/1",
+              type: "AnnotationPage",
+              items: [
+                {
+                  id: "https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/annotation/p0001-image",
+                  type: "Annotation",
+                  motivation: "painting",
+                  body: {
+                    id: "https://db.dl.tlu.ee/assets/f1667609-2d01-4c5d-98ef-7e4f3127664d",
+                    type: "Image",
+                    format: "image/jpeg",
+                    height: 1800,
+                    width: 1200,
+                  },
+                  target:
+                    "https://preview.iiif.io/cookbook/master/recipe/0001-mvm-image/canvas/p1",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    router.get("/", (req, res) => res.send("IIIF"));
+    router.get("/manifest/file/file_id", (req, res) => res.send(jsonObject));
+  },
 };
