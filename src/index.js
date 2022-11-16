7f9a36d19f6fdc0966b1b50006c5c1cd3cbf76a5
Hembo Pagi <95413906+HemboPagi@users.noreply.github.com>
2022-11-16 15:58:47 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 // sisse tulev väärtus on faili ID
 // selle järgi pärida faili mõõtmed directus_files tabelist ning asendada height ja width väärtused
 // kas api väljund cachetakse kuidagi? tegelit poleks vaja ju uusi päringuid teha alati ...
@@ -7,75 +6,74 @@
 // https://docs.directus.io/extensions/endpoints.html
 // https://docs.directus.io/extensions/creating-extensions.html
 
+import { useApi } from "@directus/extensions-sdk";
 
-import { useApi } from '@directus/extensions-sdk';
-
-const axios = require('axios');
-const createIifJson = (fileId, height, width) => (
+const axios = require("axios");
+const createIifJson = (fileId, height, width) => ({
+  "@context": "http://iiif.io/api/presentation/3/context.json",
+  id: `https://db.dl.tlu.ee/iiif/manifest/file/${fileId}`, //replace <file_id> with the real value
+  type: "Manifest",
+  label: {
+    en: ["Image"],
+  },
+  rights: "http://creativecommons.org/licenses/by/4.0/",
+  items: [
     {
-      "@context": "http://iiif.io/api/presentation/3/context.json",
-      id: `https://db.dl.tlu.ee/iiif/manifest/file/${fileId}`, //replace <file_id> with the real value
-      type: "Manifest",
-      label: {
-        en: ["Image"],
-      },
-      rights: "http://creativecommons.org/licenses/by/4.0/",
+      id: "https://db.dl.tlu.ee/iiif/canvas",
+      type: "Canvas",
+      height: `${height}`, //replace with real values
+      width: `${width}`, //replace with real values
       items: [
         {
-          id: "https://db.dl.tlu.ee/iiif/canvas",
-          type: "Canvas",
-          height: `${height}`,//replace with real values
-          width: `${width}`,//replace with real values
+          id: "https://db.dl.tlu.ee/iiif/image/page",
+          type: "AnnotationPage",
           items: [
             {
-              id: "https://db.dl.tlu.ee/iiif/image/page",
-              type: "AnnotationPage",
-              items: [
-                {
-                  id: "https://db.dl.tlu.ee/iiif/image",
-                  type: "Annotation",
-                  motivation: "painting",
-                  body: {
-                    id: `https://db.dl.tlu.ee/assets/${fileId}?format=jpg`, //replace file ID with the real value and lets make sure it is JPG by using format=jpg
-                    type: "Image",
-                    format: "image/jpeg",
-                    height: `${height}`, //replace with real values
-                    width: `${width}`,//replace with real values
-                  },
-                  target:
-                      "https://db.dl.tlu.ee/iiif/canvas",
-                },
-              ],
+              id: "https://db.dl.tlu.ee/iiif/image",
+              type: "Annotation",
+              motivation: "painting",
+              body: {
+                id: `https://db.dl.tlu.ee/assets/${fileId}?format=jpg`, //replace file ID with the real value and lets make sure it is JPG by using format=jpg
+                type: "Image",
+                format: "image/jpeg",
+                height: `${height}`, //replace with real values
+                width: `${width}`, //replace with real values
+              },
+              target: "https://db.dl.tlu.ee/iiif/canvas",
             },
           ],
         },
       ],
-    }
-)
+    },
+  ],
+});
 
 export default {
   setup() {
-		const api = useApi();
-
-    //const { Directus } = require('@directus/sdk');
-
-    const directus = new api('http://directus.example.com');
-    
-    const real_estate = directus.items('real_estate');
-    console.log(real_estate);
-
-	},
+    const api = useApi();
+  },
   id: "iiif",
   handler: (router) => {
+    const loadItem = async () => {
+      try {
+        const item = await api.get(
+          `/files/f1667609-2d01-4c5d-98ef-7e4f3127664d?fields=width`
+        );
+        pageContent.value = item.data.width;
+      } finally {
+        loading.value = false;
+      }
+    };
+    console.log(loadItem);
     router.get("/", (req, res) => res.send("IIIF"));
-      router.get('/manifest/file/:file_id/:file_height/:file_width', function(req, res) {
+    router.get(
+      "/manifest/file/:file_id/:file_height/:file_width",
+      function (req, res) {
         const fileId = req.params.file_id;
         const fileHeight = req.params.file_height;
-        const fileWidth = req.params.file_width
+        const fileWidth = req.params.file_width;
         res.send(createIifJson(fileId, fileHeight, fileWidth));
-      });
+      }
+    );
   },
 };
-
-
-
