d74ad50860281e35c4b0e7c4f288ad7e8c1a969e
hembo <hembo@tlu.ee>
2022-12-05 14:18:23 +0200

    itemMeta added

--- src/index.js
+++ src/index.js
@@ -6,150 +6,166 @@
 // https://docs.directus.io/extensions/endpoints.html
 // https://docs.directus.io/extensions/creating-extensions.html
 
-
 const createItemArray = (results) => {
-    const items = results.map((item, index)=> (
-        {
-            id: `https://db.dl.tlu.ee/iiif/canvas/${index + 1}`,
-            type: "Canvas",
-            height: `${item.height}`,//replace with real values
-            width: `${item.width}`,//replace with real values
-            items: [
-                {
-                    id: `https://db.dl.tlu.ee/iiif/image/page/${index + 1}`,
-                    type: "AnnotationPage",
-                    items: [
-                        {
-                            id: `https://db.dl.tlu.ee/iiif/image/${index + 1}`,
-                            type: "Annotation",
-                            motivation: "painting",
-                            body: {
-                                id: `https://db.dl.tlu.ee/assets/${item.id}?format=jpg`, //replace file ID with the real value and lets make sure it is JPG by using format=jpg
-                                type: "Image",
-                                format: "image/jpeg",
-                                height: `${item.height}`, //replace with real values
-                                width: `${item.width}`,//replace with real values
-                            },
-                            target:
-                                `https://db.dl.tlu.ee/iiif/canvas/${index + 1}`,
-                        },
-                    ],
-                },
-            ],
-        }
-    ))
-    return (
-       items
-    )
-}
-const createIiifCollectionJson = (canvasLabel, items, collection, fileId) => {
-    return (
-        {
-            "@context": "http://iiif.io/api/presentation/3/context.json",
-            "id": `https://db.dl.tlu.ee/iiif/manifest/${collection}/${fileId}`,
-            "type": "Manifest",
-            "label": {
-                "et": [
-                    `${canvasLabel}`
-                ]
-            },
-            "items": items
-        }
-    )
-}
-const createIiifSingleImageJson = (fileId, height, width) => (
-    {
-        "@context": "http://iiif.io/api/presentation/3/context.json",
-        id: `https://db.dl.tlu.ee/iiif/manifest/file/${fileId}`, //replace <file_id> with the real value
-        type: "Manifest",
-        label: {
-            en: ["Image"],
-        },
-        rights: "http://creativecommons.org/licenses/by/4.0/",
+  const items = results.map((item, index) => ({
+    id: `https://db.dl.tlu.ee/iiif/canvas/${index + 1}`,
+    type: "Canvas",
+    height: `${item.height}`, //replace with real values
+    width: `${item.width}`, //replace with real values
+    items: [
+      {
+        id: `https://db.dl.tlu.ee/iiif/image/page/${index + 1}`,
+        type: "AnnotationPage",
         items: [
-            {
-                id: "https://db.dl.tlu.ee/iiif/canvas/1",
-                type: "Canvas",
-                height: `${height}`,//replace with real values
-                width: `${width}`,//replace with real values
-                items: [
-                    {
-                        id: "https://db.dl.tlu.ee/iiif/image/page/1",
-                        type: "AnnotationPage",
-                        items: [
-                            {
-                                id: "https://db.dl.tlu.ee/iiif/image/1",
-                                type: "Annotation",
-                                motivation: "painting",
-                                body: {
-                                    id: `https://db.dl.tlu.ee/assets/${fileId}?format=jpg`, //replace file ID with the real value and lets make sure it is JPG by using format=jpg
-                                    type: "Image",
-                                    format: "image/jpeg",
-                                    height: `${height}`, //replace with real values
-                                    width: `${width}`,//replace with real values
-                                },
-                                target:
-                                    "https://db.dl.tlu.ee/iiif/canvas/1",
-                            },
-                        ],
-                    },
-                ],
-            }
+          {
+            id: `https://db.dl.tlu.ee/iiif/image/${index + 1}`,
+            type: "Annotation",
+            motivation: "painting",
+            body: {
+              id: `https://db.dl.tlu.ee/assets/${item.id}?format=jpg`, //replace file ID with the real value and lets make sure it is JPG by using format=jpg
+              type: "Image",
+              format: "image/jpeg",
+              height: `${item.height}`, //replace with real values
+              width: `${item.width}`, //replace with real values
+            },
+            target: `https://db.dl.tlu.ee/iiif/canvas/${index + 1}`,
+          },
         ],
-    }
-)
+      },
+    ],
+  }));
+  return items;
+};
+const createIiifCollectionJson = (
+  canvasLabel,
+  items,
+  collection,
+  fileId,
+  itemMeta
+) => {
+  return {
+    "@context": "http://iiif.io/api/presentation/3/context.json",
+    id: `https://db.dl.tlu.ee/iiif/manifest/${collection}/${fileId}`,
+    type: "Manifest",
+    label: {
+      et: [`${canvasLabel}`],
+    },
+    metadata: [itemMeta],
+    items: items,
+  };
+};
+const createIiifSingleImageJson = (fileId, height, width) => ({
+  "@context": "http://iiif.io/api/presentation/3/context.json",
+  id: `https://db.dl.tlu.ee/iiif/manifest/file/${fileId}`, //replace <file_id> with the real value
+  type: "Manifest",
+  label: {
+    en: ["Image"],
+  },
+  rights: "http://creativecommons.org/licenses/by/4.0/",
+  items: [
+    {
+      id: "https://db.dl.tlu.ee/iiif/canvas/1",
+      type: "Canvas",
+      height: `${height}`, //replace with real values
+      width: `${width}`, //replace with real values
+      items: [
+        {
+          id: "https://db.dl.tlu.ee/iiif/image/page/1",
+          type: "AnnotationPage",
+          items: [
+            {
+              id: "https://db.dl.tlu.ee/iiif/image/1",
+              type: "Annotation",
+              motivation: "painting",
+              body: {
+                id: `https://db.dl.tlu.ee/assets/${fileId}?format=jpg`, //replace file ID with the real value and lets make sure it is JPG by using format=jpg
+                type: "Image",
+                format: "image/jpeg",
+                height: `${height}`, //replace with real values
+                width: `${width}`, //replace with real values
+              },
+              target: "https://db.dl.tlu.ee/iiif/canvas/1",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
 
 export default {
-    id: "iiif",
-    handler: (router, {services, exceptions}) => {
-        const {ItemsService} = services;
-        const {ServiceUnavailableException} = exceptions;
+  id: "iiif",
+  handler: (router, { services, exceptions }) => {
+    const { ItemsService } = services;
+    const { ServiceUnavailableException } = exceptions;
 
-        router.get("/", (req, res) => res.send("IIIF"));
-        router.get('/manifest/file/:file_id', function (req, res, next) {
-            const fileService = new ItemsService('directus_files', {
-                schema: req.schema,
-                accountability: req.accountability
-            });
-            const fileId = req.params.file_id;
-            fileService
-                .readOne(fileId, {fields: ["width", "height"]})
-                .then((results) => res.send(createIiifSingleImageJson(fileId, results.height, results.width)))
-                .catch((error) => {
-                    return next(new ServiceUnavailableException(error.message));
-                });
+    router.get("/", (req, res) => res.send("IIIF"));
+    router.get("/manifest/file/:file_id", function (req, res, next) {
+      const fileService = new ItemsService("directus_files", {
+        schema: req.schema,
+        accountability: req.accountability,
+      });
+      const fileId = req.params.file_id;
+      fileService
+        .readOne(fileId, { fields: ["width", "height"] })
+        .then((results) =>
+          res.send(
+            createIiifSingleImageJson(fileId, results.height, results.width)
+          )
+        )
+        .catch((error) => {
+          return next(new ServiceUnavailableException(error.message));
+        });
+    });
+    router.get(
+      "/manifest/:collection/:file_id",
+      async function (req, res, next) {
+        const fileId = req.params.file_id;
+        const collection = req.params.collection;
+        const itemServiceSetting = new ItemsService("IIIF_settings", {
+          schema: req.schema,
+          accountability: req.accountability,
+        });
+        const itemServiceCollection = new ItemsService(collection, {
+          schema: req.schema,
+          accountability: req.accountability,
+        });
+        const itemServiceFiles = new ItemsService("directus_files", {
+          schema: req.schema,
+          accountability: req.accountability,
+        });
 
-        })
-        router.get('/manifest/:collection/:file_id', async function (req, res, next) {
-            const fileId = req.params.file_id;
-            const collection = req.params.collection;
-             const itemServiceSetting = new ItemsService("IIIF_settings", {
-                 schema: req.schema,
-                 accountability: req.accountability
-             });
-            const itemServiceCollection= new ItemsService(collection, {
-                schema: req.schema,
-                accountability: req.accountability
-            });
-            const itemServiceFiles = new ItemsService('directus_files', {
-                schema: req.schema,
-                accountability: req.accountability
-            });
-
-            const fieldSettings = await itemServiceSetting.readByQuery( {
-                filter: { iiif_collection: { _eq: collection} },
-            });
-            const {iiif_file, iiif_canvas_label} = fieldSettings[0];
-            const collectionData = await itemServiceCollection.readOne(fileId, {fields: [`${iiif_file}.*`, iiif_canvas_label]})
-            const imageArray = collectionData[iiif_file];
-            const canvasLabel = collectionData[iiif_canvas_label];
-            const imageDataArray = []
-             await Promise.all(imageArray.map(async (item) => {
-                const imageData = await  itemServiceFiles.readOne(item.directus_files_id, {fields: ["id", "width", "height"]})
-                imageDataArray.push(imageData)
-            }));
-            const items = createItemArray(imageDataArray);
-            res.send(createIiifCollectionJson(canvasLabel, items, collection, fileId ))
-         })
-    }
-}
+        const fieldSettings = await itemServiceSetting.readByQuery({
+          filter: { iiif_collection: { _eq: collection } },
+        });
+        const { iiif_file, iiif_canvas_label } = fieldSettings[0];
+        const collectionData = await itemServiceCollection.readOne(fileId, {
+          fields: [`${iiif_file}.*`, iiif_canvas_label],
+        });
+        const imageArray = collectionData[iiif_file];
+        const canvasLabel = collectionData[iiif_canvas_label];
+        const itemMeta = collectionData[iiif_meta];
+        const imageDataArray = [];
+        await Promise.all(
+          imageArray.map(async (item) => {
+            const imageData = await itemServiceFiles.readOne(
+              item.directus_files_id,
+              { fields: ["id", "width", "height"] }
+            );
+            imageDataArray.push(imageData);
+          })
+        );
+        const items = createItemArray(imageDataArray);
+        res.send(
+          createIiifCollectionJson(
+            canvasLabel,
+            items,
+            collection,
+            fileId,
+            itemMeta
+          )
+        );
+      }
+    );
+  },
+};
