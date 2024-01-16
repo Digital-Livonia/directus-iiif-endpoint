0fcfc118d6a06a9665c2da92b7e68c56297a423d
Hembo Pagi <95413906+HemboPagi@users.noreply.github.com>
2024-01-16 15:46:28 +0200

    update

--- src/index.js
+++ src/index.js
@@ -112,7 +112,8 @@ const createIiifSingleImageJson = (fileId, height, width) => ({
       seeAlso: [
         {
           id: "https://stacks.stanford.edu/file/fg165hz3589/fg165hz3589_3.xml",
-          label: "OCR text",
+          type: "Dataset",
+          label: { "none": ["OCR text"] },
           format: "application/xml",
           profile: "http://www.loc.gov/standards/alto/ns-v2#"
         }
